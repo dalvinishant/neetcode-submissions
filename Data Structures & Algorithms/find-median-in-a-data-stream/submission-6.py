ef026e06ac6dd5dec47857ce177530ddbfc76059
Nishant Dalvi <47778365+dalvinishant@users.noreply.github.com>
2026-04-17 23:08:37 +0530

    Add: find-median-in-a-data-stream - submission-6

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-6.py
@@ -0,0 +1,17 @@
+import heapq
+class MedianFinder:
+
+    def __init__(self):
+        self.nums = []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.nums, num)
+
+    def findMedian(self) -> float:
+        if len(self.nums) % 2 == 0:
+            print(self.nums, sum(self.nums), len(self.nums))
+            vals = heapq.nsmallest(len(self.nums)//2 + 1, self.nums)[-2:]
+            return sum(vals)/2
+        else:
+            return heapq.nsmallest(len(self.nums)//2 + 1, self.nums)[-1]
+        
