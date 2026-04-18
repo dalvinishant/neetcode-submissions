6d01c83702532d095e296ff89477aec0f76581db
Nishant Dalvi <47778365+dalvinishant@users.noreply.github.com>
2026-04-18 19:37:03 +0530

    Add: partition-equal-subset-sum - submission-2

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        return self.partitionable(0, nums, {})
+    
+    def partitionable(self, curr_sum, nums, mem):
+        if not nums:
+            return False
+
+        if curr_sum == sum(nums):
+            return True
+        
+        if curr_sum in mem:
+            return mem[curr_sum]
+
+        for i, n in enumerate(nums):
+            new_sum = curr_sum + n
+            exists = self.partitionable(new_sum, nums[:i]+nums[i+1:], mem)
+            if exists:
+                mem[curr_sum] = True
+                return True
+        mem[curr_sum] = False
+        return False
+
