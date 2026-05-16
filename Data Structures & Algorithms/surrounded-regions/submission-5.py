17790aaf3b975929c4a1789f0e9d065e43083403
Nishant Dalvi <47778365+dalvinishant@users.noreply.github.com>
2026-05-16 15:36:18 +0530

    Add: surrounded-regions - submission-5

---
+++ Data Structures & Algorithms/surrounded-regions/submission-5.py
@@ -0,0 +1,28 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if i == len(board) - 1 or j == len(board[0]) - 1 or  i == 0 or j == 0:
+                    continue
+                if board[i][j] == 'O':
+                    if self.checkIfCanBeReplaced(set(), i, j, board):
+                        board[i][j] = 'X'
+                
+    def checkIfCanBeReplaced(self, v, i, j, board):
+        if i < 0 or j < 0 or i >= len(board) or j >= len(board[0]):
+            return False
+
+        if (i, j) in v:
+            return True
+        
+        if board[i][j] == 'X':
+            return True
+
+        v.add((i,j))
+
+        t = self.checkIfCanBeReplaced(v, i-1, j, board)
+        b = self.checkIfCanBeReplaced(v, i+1, j, board)
+        l = self.checkIfCanBeReplaced(v, i, j-1, board)
+        r = self.checkIfCanBeReplaced(v, i, j+1, board)
+
+        return t and b and l and r
